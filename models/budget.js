f91156218457d47a5aed300bf70453aad80fb8e0
seant3 <116916697+seant3@users.noreply.github.com>
2022-12-08 10:52:45 -0500

    budget list is populating the title

--- models/budget.js
+++ models/budget.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const budgetSchema = new Schema(
-    {
-        title: String
-    }
-)
+const budgetSchema = new Schema({
+        title: {
+            type: String,
+            required: true
+    },
+})
 
 module.exports = mongoose.model("Budget", budgetSchema);
--- routes/budgets.js
+++ routes/budgets.js
@@ -1,10 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const { route } = require('.');
+const router = express.Router();
 const budgetCtrl = require('../controllers/budgets');
 const isLoggedIn = require('../config/auth')
 
-router.get('/', budgetCtrl.index);
 router.get('/new', budgetCtrl.new);
+router.get('/', budgetCtrl.index);
 router.post('/', budgetCtrl.create);
 
 module.exports = router;
